e8fcf3500ac8569900711e9ccddc758336d85c9b
hoang.nguyenhuy <hoang.nguyenhuy@mobifone.vn>
2024-08-30 17:05:41 +0700

    Feature: authen

--- src/db/schemas/user.ts
+++ src/db/schemas/user.ts
@@ -4,7 +4,9 @@ export const users = mysqlTable('users', {
   id: serial('id').primaryKey(),
   fullName: text('full_name'),
   phone: varchar('phone', { length: 256 }),
-  email: varchar('email', { length: 256 })
+  email: varchar('email', { length: 256 }).notNull(),
+  password: varchar('password', { length: 256 }).notNull(),
+  salt: text('salt').notNull()
 })
 
 export type User = typeof users.$inferSelect
--- src/services/users.services.ts
+++ src/services/users.services.ts
@@ -14,6 +14,15 @@ export const updateUserById = async (id: number, updateUser: User) => {
   return await db.update(users).set(updateUser).where(eq(users.id, id))
 }
 
+export const getUserById = async (id: number) => {
+  const [user] = await db.select().from(users).where(eq(users.id, id))
+  return user
+}
+
+export const getUserByEmail = async (email: string) => {
+  return await db.select().from(users).where(eq(users.email, email))
+}
+
 export const deleteUserById = async (id: number) => {
   return await db.delete(users).where(eq(users.id, id))
 }
